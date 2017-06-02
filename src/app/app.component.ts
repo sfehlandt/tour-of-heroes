7e12005fbe5c6d0d03b59c052f6bbc7e808068cc
Sebastian <sebafehlandt@gmail.com>
2017-06-02 17:28:39 -0400

    Add Master/Detail view on clic selection of a hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,26 +24,31 @@ const HEROES: Hero[] = [
     styleUrls: ['./app.component.css'],
     template: `
         <h1>{{title}}</h1>
-        <h2>{{hero.name}} details!</h2>
-        <div><label>id: </label>{{selectedHero.id}}</div>
-        <div>
-            <label>name: </label>
-            <input [(ngModel)]="hero.name" placeholder="name">
-        </div>
-
         <h2>My Heroes</h2>
         <ul class="heroes">
-            <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+            <li *ngFor="let hero of heroes"
+                [class.selected]="hero === selectedHero"
+                (click)="onSelect(hero)">
                 <span class="badge">{{hero.id}}</span> {{hero.name}}
             </li>
         </ul>
+
+        <div *ngIf="selectedHero">
+            <h2>{{selectedHero.name}} details!</h2>
+            <div><label>id: </label>{{selectedHero.id}}</div>
+            <div>
+                <label>name: </label>
+                <input [(ngModel)]="selectedHero.name" placeholder="name">
+            </div>
+        </div>
     `
 })
 export class AppComponent {
     title = 'Tour of Heroes';
     heroes = HEROES;
-    hero : Hero = {
-        id: 1,
-        name: 'Windsorm'
-    };
+    selectedHero : Hero;
+
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
 }
