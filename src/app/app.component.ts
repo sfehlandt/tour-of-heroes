49aca0e78801ca69bf293dc771f2c474843a321b
sfehlandt <sebafehlandt@gmail.com>
2017-06-04 23:07:43 -0400

    Add Hero Service to provide HEROES mock data as an immediate promise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+import { HeroService } from './hero.service';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService],
 })
 
-export class AppComponent {
-    title = 'Tour of Heroes';
-    heroes = HEROES;
-    selectedHero : Hero;
+export class AppComponent implements OnInit {
+  title = 'Tour of Heroes';
+  heroes: Hero[];
+  selectedHero : Hero;
 
-    onSelect(hero: Hero): void {
-        this.selectedHero = hero;
-    }
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
